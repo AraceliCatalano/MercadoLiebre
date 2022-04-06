abcbecde541f0a7fd44dd26e2e393df2081018b4
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-04-05 22:56:51 -0300

    actualizo puerto

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3002, ()=>{
-    console.log('Servidor ejecutando en puerto 3002');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor ejecutando en puerto 3000');
 });
 
 app.get('/', (req,res)=>{
